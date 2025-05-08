c7177864da9647b8fa937836b17cb9e7504ef89d
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-08 20:30:47 +0700

    tambahkan logika penyisipan node di awal list dan validasi duplikat

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -25,3 +25,13 @@ void addNode() {
 
     Node *nodeBaru = new Node;
     nodeBaru->noMhs = nim;
+
+    if (START == NULL || nim <= START->noMhs) {
+        if ((START != NULL) && (nim == START->noMhs)) {
+            cout << "\nDuplikasi noMhs tidak diijinkan\n";
+            return;
+        }
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
+    }
