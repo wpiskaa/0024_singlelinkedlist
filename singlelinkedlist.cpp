e582b738e2f46acd46b51110dbd877cded74731f
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-08 20:31:47 +0700

    tambahkan pencarian posisi penyisipan dan validasi duplikat dalam list

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -35,3 +35,14 @@ void addNode() {
         START = nodeBaru;
         return;
     }
+    Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs)) {
+            if (nim == current->noMhs) {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
