bd2b0e42d5cc0d1c67916ce9d9dc998bf1678c46
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-08 20:38:35 +0700

    tambahkan fungsi untuk menghapus node berdasarkan nim

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -64,3 +64,17 @@ void addNode() {
         }
         return (*current != NULL);
     }
+    bool delNode(int nim) {
+        Node *current, *previous;
+        if (!Search(nim, previous, current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+    
