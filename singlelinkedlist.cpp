5bfa4d45e42a553415361f45873100750756a8a4
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-08 20:29:38 +0700

    tambahkan input dan inisialisasi node baru untuk mahasiswa

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-class Node {
-    public:
-        int noMhs;
-        Node *next;
-    };
-   
-    class LinkedList {
-        Node *START;
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
     }
+}
+void addNode() {
+    int nim;
+    cout << "\nMasukkan Nomor Mahasiswa: ";
+    cin >> nim;
+
+    Node *nodeBaru = new Node;
+    nodeBaru->noMhs = nim;
