358571922796f84efacab781d4e3bfbfa0d6a734
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-08 20:34:51 +0700

    tambahkan fungsi untuk cek list kosong dan mencari data berdasarkan nim

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -50,3 +50,17 @@ void addNode() {
         nodeBaru->next = current;
         previous->next = nodeBaru;
     }
+    bool listEmpty() {
+        return (START == NULL);
+    }
+
+    bool Search(int nim, Node *&previous, Node *&current) {
+        *previous = START;
+        *current = START;
+
+        while ((current != NULL) && (nim != (*current)->noMhs)) {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+        return (*current != NULL);
+    }
